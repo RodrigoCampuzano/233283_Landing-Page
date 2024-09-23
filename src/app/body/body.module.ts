2c3984e508f0bb29287ab4ed86644e505a1589ab
Rodrigo Campuzano <camcluod@gmail.com>
2024-09-22 23:31:11 -0600

    Creacion de los modulos

---
+++ src/app/body/body.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardsDashboardComponent } from './cards-dashboard/cards-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardsDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BodyModule { }
---
+++ src/app/footer/footer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterDashboardComponent } from './footer-dashboard/footer-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FooterModule { }
